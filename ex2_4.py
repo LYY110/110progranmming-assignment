22bfdef97c4cb96585a6294d32c2e46e8dc06517
LYY110 <43258806+LYY110@users.noreply.github.com>
2018-10-13 21:06:31 +0800

    实验作业

---
+++ ex2_4.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.setup(650,350,200,200)
+turtle.penup()
+turtle.fd(-60)
+turtle.pendown()
+turtle.pensize(6)
+turtle.pencolor("green")
+turtle.fd(120)
+turtle.seth(120)
+turtle.fd(120)
+turtle.left(120)
+turtle.fd(120)

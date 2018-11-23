80d333a5fa465aec13fc4c8e78e06ecb6baea852
LYY110 <43258806+LYY110@users.noreply.github.com>
2018-11-23 09:10:32 +0800

    实验作业

---
+++ ex5_6.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+now=datetime.now()
+now.strftime("%Y-%m-%d")
+now.strftime("%b %d, %Y")
+now.strftime("%d %B %Y")
+now.strftime("%Y/%m/%d")
+now.strftime("%m/%d/%Y")
+now.strftime("%A/%Y/%m/%d")
